b4ac7ca51c630f75127cc6a1c14dcd0061f42e0f
Said-Akbar <you@example.com>
2024-12-24 22:17:34 -0800

    [gfx906] Added AMD MI25/50/60

--- third_party/amd/lib/TritonAMDGPUToLLVM/TargetUtils.cpp
+++ third_party/amd/lib/TritonAMDGPUToLLVM/TargetUtils.cpp
@@ -19,6 +19,10 @@ ISAFamily deduceISAFamily(llvm::StringRef arch) {
     return ISAFamily::CDNA2;
   case llvm::AMDGPU::GK_GFX908:
     return ISAFamily::CDNA1;
+  case llvm::AMDGPU::GK_GFX900:
+  case llvm::AMDGPU::GK_GFX902:
+  case llvm::AMDGPU::GK_GFX906:
+    return ISAFamily::GCN5;
   default:
     break;
   }
